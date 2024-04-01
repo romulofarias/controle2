9386eec82b3bdfdfb38138931340bd51723cd8b9
Rômulo Brito de Farias <fariasbrito.romulo@gmail.com>
2024-04-01 14:33:51 -0300

    organização do código

--- codigos/gnu.octave/appControle.m
+++ codigos/gnu.octave/appControle.m
@@ -21,6 +21,9 @@ h.monitor_panel = uipanel("title","Monitor","position",[0.02 0.06 0.38 0.74],"un
 #painel de gráficos
 h.graphic_panel = uipanel("title","Gráficos","position",[0.402 0.06 0.58 0.92],"units","normalized");
 
+#h.text = uicontrol("style","text","string","0.1368","callback","","units","normalized","position",[0.6 0.8 0.35 0.08]);
+h.btn1 = uicontrol("style","pushbutton","string","Random","callback",@HandlerControl,"units","normalized","position",[0.77 0.02 0.1 0.04]);
+h.btn2 = uicontrol("style","pushbutton","string","Sair","callback",@HandlerControl,"units","normalized","position",[0.88 0.02 0.1 0.04]);
 #
 function HandlerControl(obj,init = false)
     h = guidata(obj)
@@ -35,10 +38,6 @@ function HandlerControl(obj,init = false)
             set(h.select_L1,"value",0);
     end
 end
-
-#h.text = uicontrol("style","text","string","0.1368","callback","","units","normalized","position",[0.6 0.8 0.35 0.08]);
-h.btn1 = uicontrol("style","pushbutton","string","Random","callback",@HandlerControl,"units","normalized","position",[0.77 0.02 0.1 0.04]);
-h.btn2 = uicontrol("style","pushbutton","string","Sair","callback",@HandlerControl,"units","normalized","position",[0.88 0.02 0.1 0.04]);
 set(gcf,"color",get(0,"defaultuicontrolbackgroundcolor"));
 guidata(h.f,h);
 HandlerControl(gcf,true);
