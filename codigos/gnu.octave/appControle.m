e09ecf98fb52a3ee48411a66d37fe82c09ad9234
Rômulo Brito de Farias <fariasbrito.romulo@gmail.com>
2024-04-02 18:11:27 -0300

    paineis graficos inseridos

--- codigos/gnu.octave/appControle.m
+++ codigos/gnu.octave/appControle.m
@@ -20,6 +20,26 @@ h.select_L2 = uicontrol(h.ctrlPanel,"units","normalized","style","radiobutton","
 h.monitor_panel = uipanel("title","Monitor","position",[0.02 0.06 0.38 0.74],"units","normalized");
 #painel de gráficos
 h.graphic_panel = uipanel("title","Gráficos","position",[0.402 0.06 0.58 0.92],"units","normalized");
+#gráfico da resposta autêntica do sistema
+h.g_real = uipanel(h.graphic_panel,"title","Real","position",[0 0.5 1 0.498],"units","normalized","titleposition","centertop");
+h.ax1 = axes(h.g_real, "position",[0.13 0.15 0.775 0.815]);
+h.gp1 = uibuttongroup(h.g_real, "position",[0 0 1 0.08]);
+h.gp1_ckb1 = uicontrol(h.gp1, "units","normalized","style","checkbox","string","L1","position",[0.1 0.3 0.5 0.5]);
+h.gp1_ckb2 = uicontrol(h.gp1, "units","normalized","style","checkbox","string","L2","position",[0.3 0.3 0.5 0.5]);
+h.gp1_ckb3 = uicontrol(h.gp1, "units","normalized","style","checkbox","string","SetPoint","position",[0.5 0.3 0.5 0.5]);
+h.gp1_ckb4 = uicontrol(h.gp1, "units","normalized","style","checkbox","string","Controle","position",[0.7 0.3 0.5 0.5]);
+h.gp1_ckb5 = uicontrol(h.gp1, "units","normalized","style","checkbox","string","Erro","position",[0.9 0.3 0.5 0.5]);
+
+
+#gráfico da resposta simulada do sistema
+h.g_sim = uipanel(h.graphic_panel,"title","Simulado","position",[0 0 1 0.5],"units","normalized","titleposition","centertop");
+h.ax2 = axes(h.g_sim, "position",[0.13 0.15 0.775 0.815]);
+h.gp2 = uibuttongroup(h.g_sim, "position",[0 0 1 0.08]);
+h.gp2_ckb1 = uicontrol(h.gp2, "units","normalized","style","checkbox","string","L1","position",[0.1 0.3 0.5 0.5]);
+h.gp2_ckb2 = uicontrol(h.gp2, "units","normalized","style","checkbox","string","L2","position",[0.3 0.3 0.5 0.5]);
+h.gp2_ckb3 = uicontrol(h.gp2, "units","normalized","style","checkbox","string","SetPoint","position",[0.5 0.3 0.5 0.5]);
+h.gp2_ckb4 = uicontrol(h.gp2, "units","normalized","style","checkbox","string","Controle","position",[0.7 0.3 0.5 0.5]);
+h.gp2_ckb5 = uicontrol(h.gp2, "units","normalized","style","checkbox","string","Erro","position",[0.9 0.3 0.5 0.5]);
 
 #h.text = uicontrol("style","text","string","0.1368","callback","","units","normalized","position",[0.6 0.8 0.35 0.08]);
 h.btn1 = uicontrol("style","pushbutton","string","Random","callback",@HandlerControl,"units","normalized","position",[0.77 0.02 0.1 0.04]);
